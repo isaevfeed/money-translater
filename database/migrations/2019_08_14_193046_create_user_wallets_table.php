f6ecf21e1bdd5e1b54e6009c8d942a60934cbaf2
Michael Isaev <isaevfeed@gmail.com>
2019-08-14 22:41:25 +0300

    Added migrations and fixed string length

---
+++ database/migrations/2019_08_14_193046_create_user_wallets_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserWalletsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_wallets', function (Blueprint $table) {
+            $table->integer('id')->autoIncrement();
+            $table->integer('user_id');
+            $table->tinyInteger('money');
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_wallets');
+    }
+}
